a962f1958e1b7acda5a5c2d692da94005c7bc5ef
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-02-09 17:37:38 +0100

    Update TD3_Plaisant_Matuszenski.ml

--- TD3/TD3_Plaisant_Matuszenski.ml
+++ TD3/TD3_Plaisant_Matuszenski.ml
@@ -4,17 +4,26 @@ let l : contact list = ["Felleisen"; "Leroy"; "Abelson"; "Sussman"];;
 
 let l’ : contact list = ["Abelson"; "Felleisen"; "Leroy"; "Sussman"];;
 
-(*1/ Tri par sélection*)
+
+(* ╺┓          ╺┳╸┏━┓╻   ┏━┓┏━┓┏━┓   ┏━┓╻  ┏━╸┏━╸╺┳╸╻┏━┓┏┓╻ *)
+(*  ┃    ╺━╸    ┃ ┣┳┛┃   ┣━┛┣━┫┣┳┛   ┗━┓┃  ┣╸ ┃   ┃ ┃┃ ┃┃┗┫ *)
+(* ╺┻╸          ╹ ╹┗╸╹   ╹  ╹ ╹╹┗╸   ┗━┛┗━╸┗━╸┗━╸ ╹ ╹┗━┛╹ ╹ *)
+
+
 
 (* Trie l en suivant l’ordre lexicographique. *)
 let rec sort_contacts (l : contact list) : contact list =
     if l = [] then []
     else let (m, o) = select_min l
     in m :: sort_contacts o;;
-    
-(*Cette implémentation s’appuie sur l’existence d’une fonction select_mi*)
+
+(* ⣏⡉ ⢇⡸ ⣏⡉ ⣏⡱ ⡎⠑ ⡇ ⡎⠑ ⣏⡉   ⢺    ⢺  *)
+(* ⠧⠤ ⠇⠸ ⠧⠤ ⠇⠱ ⠣⠔ ⠇ ⠣⠔ ⠧⠤   ⠼⠄ ⠶ ⠼⠄ *)
 
 let rec select_min (contlist : contact list) : (contact*(contact list)=
      match contlist with
-     |[] -> (None,[])
-     |hd :: tl -> if hd' <= hd -> hd then hd :: l;; 
+     | [] -> (None,[])
+     | hd :: tl -> if hd' <= hd -> hd then hd :: l;; 
+
+
+

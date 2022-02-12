c919e71fd75182599380b55eb8bd10dfc9361605
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-02-12 16:14:26 +0100

    Update TD3_Plaisant_Matuszenski.ml

--- TD3/TD3_Plaisant_Matuszenski.ml
+++ TD3/TD3_Plaisant_Matuszenski.ml
@@ -20,12 +20,12 @@ let rec sort_contacts (l : contact list) : contact list =
 (* ⣏⡉ ⢇⡸ ⣏⡉ ⣏⡱ ⡎⠑ ⡇ ⡎⠑ ⣏⡉   ⢺    ⢺  *)
 (* ⠧⠤ ⠇⠸ ⠧⠤ ⠇⠱ ⠣⠔ ⠇ ⠣⠔ ⠧⠤   ⠼⠄ ⠶ ⠼⠄ *)
 
-let rec select_min (contlist : contact list) : (contact*(contact list))=
+let rec select_min (contlist : contact list) : (contact * (contact list))=
     let rec min (current_min : contact) (contlist : contact list) : (contact * (contact list)) =
         match contlist with
         | [] -> (current_min, contlist)
-        | _ -> select_min ((if current_min < contlist.hd then current_min else contlist.hd); contlist.tl)
-    in (min (contlist.hd, contlist), contlist);;
+        | _ -> select_min ((if current_min < List.hd contlist then current_min else List.hd contlist); List.tl contlist)
+    in (min (List.hd contlist, contlist), contlist);;
 
 
 

47fb2c8a2be7f0bed10eacba6d56201c962712f3
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-02-12 23:26:54 +0100

    calcul des complexités

--- TD3/TD3_Plaisant_Matuszenski.ml
+++ TD3/TD3_Plaisant_Matuszenski.ml
@@ -80,8 +80,24 @@ let rec qsort_contacts (l : contact list) : contact list =
 (* ⠧⠤ ⠇⠸ ⠧⠤ ⠇⠱ ⠣⠔ ⠇ ⠣⠔ ⠧⠤   ⠮⠤ ⠶ ⠮⠤ *)
 
 (* Complexité de partition_contacts
-   La fonction partition_contacts est en fait un wrapper pour la fonction separate.
-   A chaque appel, cette fonction place un élément de son argument others
+   La fonction partition_contacts est en fait un wrapper pour la fonction
+   separate.  A chaque appel, cette fonction place un élément de son argument
+   others (qui contient les contacts qui restent à partitionner), soit dans
+   alpha, soit dans beta, et fait un appel récursif à elle-même.  La complexité
+   algorithmique de partition_contacts en fonction de la longueur de la liste
+   de contacts donnée en entrée est O(n), car elle fera n appels récursif à sa
+   sous-fonction.  Cette complexité est toujours la même, indépendamment des
+   paramètres (c'est donc la complexité minimale, moyenne et maximale à la
+   fois).
+ *)
+
+(* Complexité de qsort_contacts
+   La fonction qsort_contact est une fonction récursive qui, à chaque appel,
+   partitionne la liste de contacts en suivant un pivot (qui est le premier
+   élément de la liste), trie chaque partition récursivement, puis les joint.
+   Dans le pire des cas, Si la liste est déjà triée (ou bien triée en ordre inverse), il faudra que qsort_contacts fasse partitionner n fois la liste par partition_contacts, qui à une complexité en O(n). Donc, la comlexité dans le pire des cas est O(n^2) car un algorithme de complexité O(n^2) est exécuté n fois.
+   Dans un cas moyen (ce qui nous intéresse le plus), la partition sera faîte, en moyenne sur la moitié de la liste. On aura donc O(log n) partitions à faire. Chaque partition a une complexité de O(n), on a donc une complexité de O(n log n) dans le cas moyen.
+   Dans le meilleur des cas, le pivot permet toujours de partitionner la liste de contacts en deux parties de même longueur, ce qui fait que le nombre de divisions nécessaires pour arriver au cas de base de la récursion est O(log(n)). La complexité dans ce cas est donc O(n log(n))
  *)
 
 

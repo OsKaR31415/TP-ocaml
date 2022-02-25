2bb908c5243282ace3c6b269763200fb4313e13d
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-02-25 08:17:46 +0100

    Update TD4_Plaisant_Matuszenski.ml

--- TD4/TD4_Plaisant_Matuszenski.ml
+++ TD4/TD4_Plaisant_Matuszenski.ml
@@ -12,16 +12,66 @@ let cl: contact list = ["Fellesein"; "Leroy"; "Abelson"; "Sussman"];;
 (* ⣏⡉ ⢇⡸ ⣏⡉ ⣏⡱ ⡎⠑ ⡇ ⡎⠑ ⣏⡉   ⢺    ⢺  *)
 (* ⠧⠤ ⠇⠸ ⠧⠤ ⠇⠱ ⠣⠔ ⠇ ⠣⠔ ⠧⠤   ⠼⠄ ⠶ ⠼⠄ *)
 
-let count_chars (contlist: contact list): int =
-    let rec aux (contlist: contact list) (acc: int): int =
-        match contlist with
-        | [] -> acc
-        | hd::tl -> (aux (tl) (String.length hd))
-    in (aux contlist 1);;
+let rec count_chars (contlist: contact list): int =
+    match contlist with
+    | [] -> 0
+    | hd::tl -> (String.length hd) + count_chars tl
+
+
+(* tests unitaires *)
+assert (count_chars [] = 0);;
+assert (count_chars [""] = 0);;
+assert (count_chars ["Sussman"] = 7);;
+assert (count_chars ["Sussman"; "Leroy"] = 7 + 5);;
+assert (count_chars ["12"; "345"; "6"; ""; "7"] = 7);;
+assert (count_chars cl = 9 + 5 + 7 + 7);;
 
 
 (* ⣏⡉ ⢇⡸ ⣏⡉ ⣏⡱ ⡎⠑ ⡇ ⡎⠑ ⣏⡉   ⢺    ⠊⡱ *)
 (* ⠧⠤ ⠇⠸ ⠧⠤ ⠇⠱ ⠣⠔ ⠇ ⠣⠔ ⠧⠤   ⠼⠄ ⠶ ⠮⠤ *)
+
+let rec count_chars_term (contlist: contact list) (acc: int): int =
+        match contlist with
+        | [] -> acc
+        | hd::tl -> (count_chars_term (tl) (acc + (String.length hd)));;
+
+
+(* tests unitaires *)
+assert (count_chars_term []                          0 = 0);;
+assert (count_chars_term [""]                        0 = 0);;
+assert (count_chars_term ["Sussman"]                 0 = 7);;
+assert (count_chars_term ["Sussman"; "Leroy"]        0 = 7 + 5);;
+assert (count_chars_term ["12"; "345"; "6"; ""; "7"] 0 = 7);;
+assert (count_chars_term cl                          0 = 9 + 5 + 7 + 7);;
+
+
+
+(* ⣏⡉ ⢇⡸ ⣏⡉ ⣏⡱ ⡎⠑ ⡇ ⡎⠑ ⣏⡉   ⢺    ⢉⡹ *)
+(* ⠧⠤ ⠇⠸ ⠧⠤ ⠇⠱ ⠣⠔ ⠇ ⠣⠔ ⠧⠤   ⠼⠄ ⠶ ⠤⠜ *)
+
+let count_chars (contlist: contact list): int =
+    count_chars_term contlist 0;;
+
+(* ⣏⡉ ⢇⡸ ⣏⡉ ⣏⡱ ⡎⠑ ⡇ ⡎⠑ ⣏⡉   ⢺    ⢇⣸ *)
+(* ⠧⠤ ⠇⠸ ⠧⠤ ⠇⠱ ⠣⠔ ⠇ ⠣⠔ ⠧⠤   ⠼⠄ ⠶  ⠸ *)
+
+
+let count_chars (contlist: contact list): int =
+    let rec aux (contlist: contact list) (acc: int): int =
+        match contlist with
+        | [] -> acc
+        | hd::tl -> (aux tl (acc + String.length hd))
+    in aux contlist 0;;
+
+
+(* tests unitaires *)
+assert (count_chars [] = 0);;
+assert (count_chars [""] = 0);;
+assert (count_chars ["Sussman"] = 7);;
+assert (count_chars ["Sussman"; "Leroy"] = 7 + 5);;
+assert (count_chars ["12"; "345"; "6"; ""; "7"] = 7);;
+assert (count_chars cl = 9 + 5 + 7 + 7);;
+
 
 
 

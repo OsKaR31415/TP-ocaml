6913359062789057e543fa84c8efba558b6e7ff5
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-03-14 10:44:01 +0100

    Exercice 2.2, 2.3, 2.4 du TD6

--- TD6/TD6_Dezzaz-Matuszenski-Plaisant.ml
+++ TD6/TD6_Dezzaz-Matuszenski-Plaisant.ml
@@ -109,3 +109,71 @@ let rec bst_nodes (tree: 'a bintree) : 'a list =
 bst_nodes BEmpty = [];;
 bst_nodes t0 = [2; 3; 4; 6; 8; 10];;
 
+
+
+(* ⣏⡉ ⢇⡸ ⣏⡉ ⣏⡱ ⡎⠑ ⡇ ⡎⠑ ⣏⡉   ⠊⡱   ⠊⡱ *)
+(* ⠧⠤ ⠇⠸ ⠧⠤ ⠇⠱ ⠣⠔ ⠇ ⠣⠔ ⠧⠤   ⠮⠤ ⠶ ⠮⠤ *)
+
+let rec add_node (tree: 'a bintree) (node: 'a) : 'a bintree =
+    match tree with
+    | BEmpty -> BNode(node, BEmpty, BEmpty)
+    | BNode(x, left, right) -> if x > node
+            then BNode(x, (add_node left node), right)
+            else BNode(x, left, (add_node right node));;
+
+add_node t0 5 = (
+BNode (4,
+ BNode (2, BEmpty, BNode (3, BEmpty, BEmpty)),
+ BNode (8,
+  BNode (6, BNode (5, BEmpty, BEmpty), BEmpty),
+  BNode (10, BEmpty, BEmpty)))
+);;
+add_node t0 7 = (
+BNode (4,
+ BNode (2, BEmpty, BNode (3, BEmpty, BEmpty)),
+ BNode (8,
+  BNode (6, BEmpty, BNode (7, BEmpty, BEmpty)),
+  BNode (10, BEmpty, BEmpty)))
+);;
+
+let make_bst (elements : 'a list) : 'a bintree =
+    let rec aux (elements : 'a list) (tree: 'a bintree) : 'a bintree =
+        match elements with
+        | [] -> tree
+        | hd :: tl -> aux tl (add_node tree hd)
+    in aux elements BEmpty;;
+
+make_bst [1; 2; 3; 0] = (
+BNode (1, BNode (0, BEmpty, BEmpty),
+ BNode (2, BEmpty, BNode (3, BEmpty, BEmpty)))
+);;
+
+make_bst [4; 2; 3; 8; 6; 10] = t0;;
+
+
+(* ⣏⡉ ⢇⡸ ⣏⡉ ⣏⡱ ⡎⠑ ⡇ ⡎⠑ ⣏⡉   ⠊⡱   ⢉⡹ *)
+(* ⠧⠤ ⠇⠸ ⠧⠤ ⠇⠱ ⠣⠔ ⠇ ⠣⠔ ⠧⠤   ⠮⠤ ⠶ ⠤⠜ *)
+
+
+let rec bst_inserts (tree : 'a bintree) : 'a list =
+    match tree with
+    | BEmpty -> []
+    | BNode(x, left, right) -> x :: (bst_inserts left) @ (bst_inserts right);;
+
+make_bst (bst_inserts t0) = t0;;
+
+
+(* ⣏⡉ ⢇⡸ ⣏⡉ ⣏⡱ ⡎⠑ ⡇ ⡎⠑ ⣏⡉   ⠊⡱   ⢇⣸ *)
+(* ⠧⠤ ⠇⠸ ⠧⠤ ⠇⠱ ⠣⠔ ⠇ ⠣⠔ ⠧⠤   ⠮⠤ ⠶  ⠸ *)
+(* DIFFICILE *)
+
+let rec delete_node (node: 'a) (tree: 'a bintree) : 'a bintree =
+    match tree with
+    | BEmpty -> BEmpty
+    | BNode(x, left, right) -> if x = node then left
+    else BNode(x, delete_node node left, delete_node node right);;
+
+delete_node 8 t0;;
+
+
+
